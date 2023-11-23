a9306ceb33554c434b1c2600491b6e39371d0075
Amel <am.benkhelifa03@gmail.com>
2023-11-23 12:19:20 +0100

    deinodiezd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel Beauty Salon</title>
+    <title>Angie Cils Salon</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
